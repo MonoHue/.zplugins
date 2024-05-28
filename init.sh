0e989b6654757714c73fc983f8a42be9e1d551c8
Liangyu Li <libertyli@hotmail.com>
2024-05-28 11:43:34 +0800

    init

---
+++ init.sh
@@ -0,0 +1,5 @@
+echo "# Z-Plugins" > ~/.zshrc
+echo ". ~/.zplugins/z/z.sh" > ~/.zshrc
+
+echo "source ~/.zplugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" > ~/.zshrc
+echo "source ~/.zplugins/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.zshrc
