982bcc653743249fd61e8d232ba3f6b7e45e99bd
Liangyu Li <libertyli@hotmail.com>
2024-05-28 11:56:54 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
-echo "# Z-Plugins" > ~/.zshrc
-echo ". ~/.zplugins/z/z.sh" > ~/.zshrc
+echo "# Z-Plugins" >> ~/.zshrc
 
-echo "source ~/.zplugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" > ~/.zshrc
-echo "source ~/.zplugins/zsh-autosuggestions/zsh-autosuggestions.zsh" > ~/.zshrc
+echo "source ~/.zplugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
+echo "source ~/.zplugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
